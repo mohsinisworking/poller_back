ad46bf02dceea657e5c84bfcef91d98cc8087fde
mohsinisworking <mohsin.ashraf@contour-software.com>
2025-07-11 12:23:56 +0500

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -19,8 +19,8 @@ sample_data = [
         "options": ["Spring", "Summer", "Autumn", "Winter"]
     },
     {
-        "question": "Thoughts on Remote Work?",
-        "options": ["Should be fully implemented!", "Hybrid", "Should be BANNED"]
+        "question": "NightOwl OR EarlyBird?",
+        "options": ["NightOwl", "EarlyBird"]
     }
 ]
 for sample in sample_data:
@@ -33,9 +33,21 @@ for sample in sample_data:
     }
     time.sleep(0.01)  # ensure unique poll_id
 
+# --- SignalR negotiate endpoint ---
+@app.function_name(name="negotiate")
+@app.route(route="negotiate", methods=["POST"], auth_level=func.AuthLevel.ANONYMOUS)
+def negotiate(req: func.HttpRequest) -> func.HttpResponse:
+    connection_info = app.connection_info("pollz_signalr")
+    return func.HttpResponse(
+        json.dumps(connection_info),
+        mimetype="application/json"
+    )
+
+# --- Create Poll (with SignalR) ---
 @app.function_name(name="createPoll")
 @app.route(route="createPoll", methods=["POST"], auth_level=func.AuthLevel.ANONYMOUS)
-def create_poll(req: func.HttpRequest) -> func.HttpResponse:
+@app.signalr_output(arg_name="signalRMessages", hub_name="pollz_signalr")
+def create_poll(req: func.HttpRequest, signalRMessages: func.Out[func.SignalRMessage]) -> func.HttpResponse:
     try:
         data = req.get_json()
         question = data.get("question")
@@ -44,7 +56,6 @@ def create_poll(req: func.HttpRequest) -> func.HttpResponse:
         if not question or not options or not isinstance(options, list):
             return func.HttpResponse("Invalid input", status_code=400)
 
-        # Generate a unique poll_id (timestamp string)
         poll_id = str(int(time.time() * 1000))
         poll = {
             "poll_id": poll_id,
@@ -54,14 +65,22 @@ def create_poll(req: func.HttpRequest) -> func.HttpResponse:
         }
         polls[poll_id] = poll
 
+        # Send SignalR message to all clients
+        signalRMessages.set(func.SignalRMessage(
+            target="pollsUpdated",
+            arguments=[list(polls.values())]
+        ))
+
         return func.HttpResponse(json.dumps(poll), mimetype="application/json", status_code=201)
     except Exception as e:
         logging.error(str(e))
         return func.HttpResponse("Error creating poll", status_code=500)
 
+# --- Vote Poll (with SignalR) ---
 @app.function_name(name="votePoll")
 @app.route(route="votePoll", methods=["POST"], auth_level=func.AuthLevel.ANONYMOUS)
-def vote_poll(req: func.HttpRequest) -> func.HttpResponse:
+@app.signalr_output(arg_name="signalRMessages", hub_name="pollz_signalr")
+def vote_poll(req: func.HttpRequest, signalRMessages: func.Out[func.SignalRMessage]) -> func.HttpResponse:
     try:
         data = req.get_json()
         poll_id = data.get("poll_id")
@@ -74,11 +93,19 @@ def vote_poll(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse("Invalid option index", status_code=400)
 
         polls[poll_id]["votes"][option_index] += 1
+
+        # Send SignalR message to all clients
+        signalRMessages.set(func.SignalRMessage(
+            target="pollsUpdated",
+            arguments=[list(polls.values())]
+        ))
+
         return func.HttpResponse(json.dumps(polls[poll_id]), mimetype="application/json", status_code=200)
     except Exception as e:
         logging.error(str(e))
         return func.HttpResponse("Error voting in poll", status_code=500)
 
+# --- Get Single Poll ---
 @app.function_name(name="getPoll")
 @app.route(route="getPoll", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
 def get_poll(req: func.HttpRequest) -> func.HttpResponse:
@@ -89,9 +116,9 @@ def get_poll(req: func.HttpRequest) -> func.HttpResponse:
     poll = polls[poll_id]
     return func.HttpResponse(json.dumps(poll), mimetype="application/json")
 
+# --- Get All Polls ---
 @app.function_name(name="getAllPolls")
 @app.route(route="getAllPolls", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
 def get_all_polls(req: func.HttpRequest) -> func.HttpResponse:
-    # Return all poll objects (not just id/question)
     poll_list = list(polls.values())
     return func.HttpResponse(json.dumps(poll_list), mimetype="application/json")
